928c9d5ba5649c987c255cf8370d92fcee88031c
Jakub Dorňák <jdornak@redhat.com>
2016-03-03 16:07:20 +0100

    Django 1.8 compatibility

--- rhw/urls.py
+++ rhw/urls.py
@@ -1,14 +1,26 @@
-from django.conf.urls import patterns, include, url
+"""rhw URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
 from django.contrib.auth.decorators import login_required as lr, permission_required as pr
 from django.contrib.auth.views import login, logout
-from django.contrib import admin
-admin.autodiscover()
 
 from .views import *
 from .forms import PasswordResetForm
 
-urlpatterns = patterns('',
-
+urlpatterns = [
     url(r'^$',                                              HomeView.as_view(),                 name='home'),
     url(r'^ideas/$',                                        IdeasView.as_view(),                name='ideas'),
     url(r'^ideas/new/$',                                    lr(IdeaCreateView.as_view()),       name='idea-create'),
@@ -34,5 +46,4 @@ urlpatterns = patterns('',
     url(r'^auth/',      include('django.contrib.auth.urls')),
     url(r'^admin/',     include(admin.site.urls)),
     url(r'^ckeditor/',  include('ckeditor_uploader.urls')),
-)
-
+]
